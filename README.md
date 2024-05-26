9ebd8565b3d1c656a9690ba0693226d011d462df
dm123321_31md <dm12332131mdgaming@gmail.com>
2024-05-26 12:46:26 -0400

    soda push

---
+++ README.md
@@ -0,0 +1,13 @@
+# Soda
+
+## Description
+
+hai!
+dis soda
+drink it
+
+its safe
+
+## Installation
+
+Not sure yet
---
+++ compile.sh
@@ -0,0 +1,4 @@
+sass src/main.scss out/soda.css
+sass --style=compressed src/main.scss out/soda.min.css
+cp out/soda.css out/soda.scss
+cp out/soda.min.css out/soda.min.scss
